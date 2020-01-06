bdcd4acd1efd7c1db56dd24f00b392156f730f3d
fbettega <francois.bettega@gmail.com>
2020-01-06 17:56:33 +0100

    pre traitement 2

--- script/pre_tt.R
+++ script/pre_tt.R
@@ -1,9 +1,12 @@
-
+library("tidyverse")
+library("lubridate")
 pre_tt_PA <- function(x){
 PA <- read.csv(x)  
-PA_SBP_zero<- PA %>% group_by(UNID) %>% summarise(n=n(),sum(SBP == 0))%>% ungroup()
+PA$Time <- dmy_hms(as.character(PA$Time))
+PA_without_zero <- PA  %>% filter(SBP != 0,DBP != 0)
 
-return(PA_SBP_zero)
+
+return(PA_without_zero)
 }
 
 
