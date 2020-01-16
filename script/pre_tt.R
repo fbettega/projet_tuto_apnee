06c6ff23222435995be33da6ffed767e27c766c0
fb <francois.bettega@gmail.com>
2020-01-16 11:41:13 +0100

    prez 16/01

--- script/pre_tt.R
+++ script/pre_tt.R
@@ -3,8 +3,7 @@ library("lubridate")
 pre_tt_PA <- function(x){
 PA <- read.csv(x)  
 PA$Time <- dmy_hms(as.character(PA$Time))
-PA_without_zero <- PA  %>% filter(SBP != 0,DBP != 0)
-
+PA_without_zero <- PA  %>% filter(SBP != 0,DBP != 0) %>% mutate(MonDay=replace(MonDay, MonDay==-364, 1)) %>% mutate(MonDay=replace(MonDay, MonDay==-363, 2))
 
 return(PA_without_zero)
 }
