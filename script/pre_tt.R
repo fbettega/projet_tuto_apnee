0df99b2a0585769c70e28f152730aa7a3a6c3251
fb <francois.bettega@gmail.com>
2020-02-02 19:32:25 +0100

    ajout func a la minute

--- script/pre_tt.R
+++ script/pre_tt.R
@@ -14,11 +14,11 @@ return(PA_without_zero)
 
 longue_data_heures <- function(x){
     df <- pre_tt_PA(x)
-    Systo <- df %>% mutate(heure_mesure = hour(df$Time)) %>% select(UNID,SBP,heure_mesure) 
+    Systo <- df %>% mutate(heure_mesure = hour(df$Time)) %>% dplyr::select(UNID,SBP,heure_mesure) 
     large_systo <- Systo %>% 
       pivot_wider(names_from = heure_mesure, values_from = SBP,    values_fn = list(SBP = mean)) 
     df_res <- large_systo %>%
-      select(-UNID)  %>%
+      dplyr::select(-UNID)  %>%
       t %>%
       as.data.frame %>%
       fill(names(.), .direction ="downup") %>%
@@ -26,4 +26,57 @@ longue_data_heures <- function(x){
       as_tibble %>%
       mutate(UNID = large_systo$UNID)
   return(df_res)
+}
+
+
+sort_names <- function(data) {
+  name  <- names(data)
+  chars <- str_subset(name,"[:upper:]")
+  nums  <- name %>% 
+    as.numeric() %>% 
+    sort() %>% 
+    sprintf("`%s`", .)
+  
+  select_(data, .dots = c(chars, nums))
+}
+
+
+longue_data_minutes <- function(x){
+  df <- pre_tt_PA(x)
+
+df_over_24_hours <- df %>% group_by(UNID) %>% filter(Time < (min(Time) + days(1))) 
+
+Systo_min <- df_over_24_hours  %>% mutate(heure_mesure = hour(Time),minute_mesure = cut(minute(Time), breaks = seq(0,60,10), include.lowest = TRUE)) %>% group_by(UNID,heure_mesure,minute_mesure) %>% mutate(SBP_c = max(SBP),DBP_c = max(DBP)) %>% distinct(UNID,heure_mesure,minute_mesure,SBP_c,DBP_c) %>% ungroup() %>% mutate(mesure_time = heure_mesure +as.numeric(minute_mesure)/10 ) %>% select(-heure_mesure,-minute_mesure)
+
+
+large_systo <- Systo_min %>% 
+  select(-DBP_c) %>% 
+  pivot_wider(names_from = mesure_time, values_from = SBP_c) 
+large_diasto <- Systo_min %>% 
+  select(-SBP_c) %>% 
+  pivot_wider(names_from = mesure_time, values_from = DBP_c)     
+
+df_res_systo <- large_systo %>%
+  select(-UNID)  %>%
+  t %>%
+  as.data.frame %>%
+  fill(names(.), .direction ="downup") %>%
+  t %>%
+  as_tibble %>%      
+  rename_all(funs( c(colnames(large_systo[2:ncol(large_systo)])))) %>% 
+  mutate(UNID = large_systo$UNID) %>% 
+  {suppressWarnings(sort_names(.))}
+
+df_res_diasto <- large_diasto %>%
+  select(-UNID)  %>%
+  t %>%
+  as.data.frame %>%
+  fill(names(.), .direction ="downup") %>%
+  t %>%
+  as_tibble %>% 
+  rename_all(funs( c(colnames(large_diasto[2:ncol(large_diasto)])))) %>% 
+  mutate(UNID = large_diasto$UNID) %>% 
+  {suppressWarnings(sort_names(.))}
+
+return(list(systo = df_res_systo,diasto = df_res_diasto))
 }
