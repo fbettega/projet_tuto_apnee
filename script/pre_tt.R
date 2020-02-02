833b602e5d02780a4da2f6a3e81ec71f3b4f6842
fb <francois.bettega@gmail.com>
2020-02-02 09:41:40 +0100

    pre traitement data frame wild par heure

--- script/pre_tt.R
+++ script/pre_tt.R
@@ -11,3 +11,19 @@ return(PA_without_zero)
 
 
 
+
+longue_data_heures <- function(x){
+    df <- pre_tt_PA(x)
+    Systo <- df %>% mutate(heure_mesure = hour(df$Time)) %>% select(UNID,SBP,heure_mesure) 
+    large_systo <- Systo %>% 
+      pivot_wider(names_from = heure_mesure, values_from = SBP,    values_fn = list(SBP = mean)) 
+    df_res <- large_systo %>%
+      select(-UNID)  %>%
+      t %>%
+      as.data.frame %>%
+      fill(names(.), .direction ="downup") %>%
+      t %>%
+      as_tibble %>%
+      mutate(UNID = large_systo$UNID)
+  return(df_res)
+}
