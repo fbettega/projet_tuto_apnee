83fe9c0baf286ea9eff1ebfbb17c2f3fe84db7ea
fb <francois.bettega@gmail.com>
2020-02-19 14:05:13 +0100

    modification pic

--- script/pre_tt.R
+++ script/pre_tt.R
@@ -1,5 +1,7 @@
 library("tidyverse")
 library("lubridate")
+select <- dplyr::select
+
 pre_tt_PA <- function(x){
 PA <- read.csv(x)  
 PA$Time <- dmy_hms(as.character(PA$Time))
@@ -14,11 +16,11 @@ return(PA_without_zero)
 
 longue_data_heures <- function(x){
     df <- pre_tt_PA(x)
-    Systo <- df %>% mutate(heure_mesure = hour(df$Time)) %>% dplyr::select(UNID,SBP,heure_mesure) 
+    Systo <- df %>% mutate(heure_mesure = hour(df$Time)) %>%  select(UNID,SBP,heure_mesure) 
     large_systo <- Systo %>% 
       pivot_wider(names_from = heure_mesure, values_from = SBP,    values_fn = list(SBP = mean)) 
     df_res <- large_systo %>%
-      dplyr::select(-UNID)  %>%
+       select(-UNID)  %>%
       t %>%
       as.data.frame %>%
       fill(names(.), .direction ="downup") %>%
@@ -50,7 +52,7 @@ Systo_min <- df_over_24_hours  %>% mutate(heure_mesure = hour(Time),minute_mesur
     mutate(SBP_c = max(SBP),DBP_c = max(DBP)) %>% 
     distinct(UNID,heure_mesure,minute_mesure,SBP_c,DBP_c) %>% 
     ungroup() %>% 
-    mutate(mesure_time = heure_mesure + as.numeric(minute_mesure)/10 ) %>% select(-heure_mesure,-minute_mesure)
+    mutate(mesure_time = heure_mesure + as.numeric(minute_mesure)/10 ) %>%  select(-heure_mesure,-minute_mesure)
 
 
 
@@ -58,16 +60,16 @@ Systo_min <- df_over_24_hours  %>% mutate(heure_mesure = hour(Time),minute_mesur
 
 
 large_systo <- Systo_min %>% 
-  select(-DBP_c) %>% 
+   select(-DBP_c) %>% 
   pivot_wider(names_from = mesure_time, values_from = SBP_c)   %>% 
   {suppressWarnings(sort_names(.))}
 large_diasto <- Systo_min %>% 
-  select(-SBP_c) %>% 
+   select(-SBP_c) %>% 
   pivot_wider(names_from = mesure_time, values_from = DBP_c)  %>% 
   {suppressWarnings(sort_names(.))}    
 
 df_res_systo <- large_systo %>%
-  select(-UNID)   %>%
+   select(-UNID)   %>%
   t %>%
   as.data.frame %>%
   fill(names(.), .direction ="downup") %>%
@@ -76,7 +78,7 @@ df_res_systo <- large_systo %>%
   mutate(UNID = large_systo$UNID)
 
 df_res_diasto <- large_diasto %>%
-  select(-UNID)  %>%
+   select(-UNID)  %>%
   t %>%
   as.data.frame %>%
   fill(names(.), .direction ="downup") %>%
